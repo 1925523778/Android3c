4f52a7ce075770b93b8c49e85050f8a249d98677
1925523778 <1925523778@qq.com>
2016-03-28 14:16:57 +0800

    Initial commit

---
+++ app/src/main/java/com/test/android3c/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.test.android3c;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/3/28.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondactivity);
+    }
+}
